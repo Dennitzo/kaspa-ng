c165431563b8d1a85a8060d20cbe456db713aa52
Dennitzo <56736330+Dennitzo@users.noreply.github.com>
2026-02-19 20:10:28 +0100

    #Fix

--- core/src/core.rs
+++ core/src/core.rs
@@ -5,7 +5,7 @@ use crate::mobile::MobileMenu;
 use egui::load::Bytes;
 use egui_notify::Toasts;
 #[cfg(target_os = "linux")]
-use kaspa_wallet_core::api::TransactionsDataGetResponse;
+use kaspa_wallet_core::prelude::TransactionsDataGetResponse;
 use kaspa_wallet_core::events::Events as CoreWallet;
 use kaspa_wallet_core::storage::{Binding, Hint, PrvKeyDataInfo};
 use std::borrow::Cow;
